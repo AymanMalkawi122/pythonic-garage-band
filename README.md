094bd5cd76f9ebd1ebc59e3ad58425834755e3f3
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-05-10 18:33:25 +0300

    tests passed

---
+++ README.md
@@ -0,0 +1,17 @@
+# LAB - Class 04
+
+## Project: pythonic-garage-band
+
+### Author
+
+Ayman Malkawi
+
+### How to initialize/run application
+
+in the ubintu terminal run the following command to install all of the Python modules and packages
+
+ ```pip install -r requirements.txt```
+
+then run the following command to test the functionality of all units
+
+```pytest```
--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -2,31 +2,58 @@
 
         
 class Musician:
-    def __init__(self, name):
+    def __init__(self, name = "empty", instrument = "empty", profession="empty", solo="empty"):
         self.name = name
+        self.instrument = instrument
+        self.profession = profession
+        self.solo = solo
+
+    def __str__(self):
+        return f"My name is {self.name} and I play {self.instrument}"
+    
+    def __repr__(self):
+        return f"{self.profession} instance. Name = {self.name}"
+
+    def get_instrument(self):
+        return self.instrument
+    
+    def play_solo(self):
+        return self.solo
     
 class Guitarist(Musician):
-    def __str__(self):
-        return f"My name is {self.name} and I play guitar"
-    
-    def __repr__(self):
-        return f"Guitarist instance. Name = {self.name}"
-    
-class Bassist(Musician):
-    def __str__(self):
-        return f"My name is {self.name} and I play bass"
-     
-    def __repr__(self):
-        return f"Bassist instance. Name = {self.name}"
+    def __init__(self, name):
+        instrument = "guitar"
+        profession = "Guitarist"
+        solo = "face melting guitar solo"
+        super().__init__(name, instrument, profession, solo)
     
 class Drummer(Musician):
-    def __str__(self):
-        return f"My name is {self.name} and I play drums"
+    def __init__(self, name):
+        instrument = "drums"
+        profession = "Drummer"
+        solo = "rattle boom crash"
+        super().__init__(name, instrument, profession, solo)
     
-    def __repr__(self):
-        return f"Drummer instance. Name = {self.name}"
+class Bassist(Musician):
+    def __init__(self, name):
+        instrument = "bass"
+        profession = "Bassist"
+        solo = "bom bom buh bom"
+        super().__init__(name, instrument, profession, solo)
     
 
 class Band:
-    def __init__(self, name,list):
-        self.name=
+    instances = []
+
+    def __init__(self, name, members):
+        self.name = name
+        self.members = members
+        Band.instances.append(self)
+
+    def play_solos(self):
+        get_solo = lambda member : member.play_solo()
+        return list(map(get_solo, self.members))
+    
+    @staticmethod
+    def to_list():
+        return Band.instances
